8ad6e9adb73783ccbd91989faddd43ba6427ccbb
derya <deryabalin@gmail.com>
2023-06-05 23:24:02 +0300

    proje tamamlandı

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Switch, Route,} from 'react-router-dom';
 import KaydedilenlerListesi from './Filmler/KaydedilenlerListesi';
+import FilmListesi from './Filmler/FilmListesi';
+import Film from './Filmler/Film';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -12,6 +14,8 @@ export default function App () {
       axios
         .get('http://localhost:5001/api/filmler') // Burayı Postman'le çalışın
         .then(response => {
+          console.log(response.data)
+          setMovieList(response.data)
           // Bu kısmı log statementlarıyla çalışın
           // ve burdan gelen response'u 'movieList' e aktarın
         })
@@ -23,14 +27,24 @@ export default function App () {
   }, []);
 
   const KaydedilenlerListesineEkle = id => {
+    if(!saved.includes(id)){
+      saved.push(id);
+      setSaved([...saved])
+    }
     // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
   };
 
   return (
     <div>
-      <KaydedilenlerListesi list={[ /* Burası esnek */]} />
-
-      <div>Bu Div'i kendi Routelarınızla değiştirin</div>
+      <KaydedilenlerListesi list={saved} movieList={movieList}/>
+      <Switch>
+          <Route exact path="/">
+            <FilmListesi movies ={movieList} />
+          </Route>
+          <Route path="/filmler/:id">
+            <Film KaydedilenlerListesineEkle={KaydedilenlerListesineEkle}/>
+          </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function KaydedilenlerListesi(props) {
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
       {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+        <Link key={movie} to={`/filmler/${movie}`}>
+        <span className="saved-movie">{props.movieList.find(item =>item.id == movie).title}</span>
+        </Link>
       ))}
+      <Link to={`/`}>
       <div className="home-button">Anasayfa</div>
+      </Link>
     </div>
   );
 }
